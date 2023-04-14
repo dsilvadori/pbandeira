e4146fdd3b6852cee22788961f87bf84449e01a9
dsilvadori <devmaquinads@gmail.com>
2023-04-13 22:14:30 -0300

    Implementa página de vídeos

---
+++ wp-content/themes/pbandeira/page-videos.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<main>
+    <section class="content">
+        <div class="container">
+            <div class="cap">
+                <p>V</p>
+            </div>
+            <article>
+                <p>ocê sabe como é gostoso ler um livro, sentir aquele cheirinho que é só dele, encontrar aquela lombada na sua prateleira... Bom, eu admito que sou desses, mas, para ser justo, os vídeos têm me ajudado muito a estar mais perto de você, principalmente agora que já estou mais velhinho e não consigo mais acompanhar aquela agenda maluca de viagens para todo o lado.</p>
+                <p>Então, que tal você me acompanhar lá no meu canal? Veja aqui alguns dos vídeos que tenho produzido, sempre estimulando a utilização da Literatura para o letramento e nos processos de amadurecimento emocional das crianças e adolescentes. Espero que goste!</p>
+            </article>
+            <aside>
+                <div class="picture-band">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/PEDRO-BANDEIRA-18.png" alt="Pedro Bandeira">
+                </div>
+            </aside>
+        </div>
+    </section>
+    <section class="sec-livros videos">
+        <div class="container">
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/z7Dvi-VSfWU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/7UKWf0EBaSA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
