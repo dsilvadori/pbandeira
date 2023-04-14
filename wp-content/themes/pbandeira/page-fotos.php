39fbc4d044d98c39fc95d1bd53bda82c4161744e
dsilvadori <devmaquinads@gmail.com>
2023-04-13 22:29:52 -0300

    Implementa pagina fotos

---
+++ wp-content/themes/pbandeira/page-fotos.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+    <main>
+        <div class="row">
+            <div class="col-md-3">
+                <img src="<?php echo get_template_directory_uri() ?>/img/PEDRO-BANDEIRA-5.png" alt="">
+            </div>
+            <div class="col-md-1">
+                <div class="cap">
+                    <p>V</p>
+                </div>
+            </div>
+            <div class="col-md-2" style="padding-left:0">
+                <div class="text-desc">
+                    <p>Aqui tem fotos minhas para divulgação. Use e abuse. Só não se esqueça de dar o crédito ao fotógrafo:</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <img src="<?php echo get_template_directory_uri() ?>/img/PEDRO-BANDEIRA-30.png" alt="">
+            </div>
+            <div class="col-md-3">
+                <img src="<?php echo get_template_directory_uri() ?>/img/PEDRO-BANDEIRA-37.png" alt="">
+            </div>
+        </div>
+    </main>
+<?php get_footer(); ?>
