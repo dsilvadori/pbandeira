d6e8a73d81eb3e543e0a4d6bfd11a71ad1ecf5ef
dsilvadori <devmaquinads@gmail.com>
2023-05-17 04:12:41 -0300

    finaliza página de fotos

--- wp-content/themes/pbandeira/page-fotos.php
+++ wp-content/themes/pbandeira/page-fotos.php
@@ -5,17 +5,79 @@
             <p>Aqui tem fotos minhas para divulgação. Use e abuse. Só não se esqueça de dar o crédito ao fotógrafo.</p>
             <div style="background:url(<?php echo get_template_directory_uri() ?>/img/fundo-creditos-1.png);font-weight:400;font-size:20px;padding:16px 10px;margin:0">Fotos de Felipe Panfili</div>
             <div class="row">
-                <div class="col">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" alt="">
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" alt=""></a>
                 </div>
-                <div class="col">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" alt="">
+                <div class="col-lg-2 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-02.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-02.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-05.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-05.jpg" alt=""></a>
                 </div>
-                <div class="col">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" alt="">
+                <div class="col-lg-2 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-03.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-03.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-06.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-06.jpg" alt=""></a>
+                    
                 </div>
-                <div class="col">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/fotos-01.jpg" alt="">
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-04.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/fotos-04.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/fotos-07.jpg" class="glightbox" ><img src="<?php echo get_template_directory_uri() ?>/img/fotos-07.jpg" alt="" style="width:40%"></a>
+                </div>
+            </div>
+            <div style="background:url(<?php echo get_template_directory_uri() ?>/img/fundo-creditos-2.png);font-weight:400;font-size:20px;padding:16px 10px;margin:10px 0 0 0">Fotos de Marcus Leoni</div>
+            <div class="row">
+                <div class="col-lg-2 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-1.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-1.png" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-6.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-6.png" alt=""></a>
+                </div>
+                <!-- <div class="col-lg-1" style="padding:0"></div> -->
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-2.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-2.png" alt="" style="width:47%;margin-right:5%;margin-top:-4px;"></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-3.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-3.png" alt="" style="width:47%"></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-16.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-16.png" alt="" style="width:100%"></a>
+                </div>
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-5.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-5.png" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-8.png" class="glightbox" ><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-8.png" alt="" style="width:47%;margin-right:5%"></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-11.png" class="glightbox" ><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-11.png" alt="" style="width:47%"></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-17.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-17.png" alt=""></a>
+                </div>
+                <div class="col-lg-2 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-12.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-12.png" alt=""></a>
+                </div>
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-18.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-18.png" alt=""></a>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-30.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-30.png" alt=""></a>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-31.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-31.png" alt=""></a>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-38.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-38.png" alt=""></a>
+                </div>
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-39.png" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/PEDRO BANDEIRA-39.png" alt=""></a>
+                </div>
+            </div>
+            <div style="background:url(<?php echo get_template_directory_uri() ?>/img/fundo-creditos-3.png);font-weight:400;font-size:20px;padding:16px 10px;margin:10px 0 0 0">Fotos de Will Sandrini</div>
+            <div class="row">
+                <div class="col-lg-3 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0039.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0039.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0067.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0067.jpg" alt=""></a>
+                </div>
+                <div class="col-lg-5 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0079.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0079.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0039.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0040.jpg" alt="" style="width:59%;margin-right:5%"></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0036.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0036.jpg" alt="" style="width:35%;margin-top:-160px;"></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0048.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0048.jpg" alt="" style="width:35%;float:right;margin-top:-160px;"></a>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0078.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0078.jpg" alt=""></a>
+                    <a href="<?php echo get_template_directory_uri() ?>/img/0078.jpg" class="glightbox"><img src="<?php echo get_template_directory_uri() ?>/img/0080.jpg" alt=""></a>
                 </div>
             </div>
         </div>
